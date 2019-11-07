0b7c42f3a976bdd7d72cb1889422bd8191e5194c
Gregory D Hardy <gdhardy1@gmail.com>
2019-11-07 15:47:29 -0800

    add UrlModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { UrlController } from './url/url.controller';
+import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [ConfigModule],
-  controllers: [AppController],
+  imports: [ConfigModule, UrlModule],
+  controllers: [AppController, UrlController],
   providers: [AppService],
 })
 export class AppModule {}
