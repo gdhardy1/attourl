1450232b101bbcde30bf37ee12c03d6540e6e26f
Gregory D Hardy <gdhardy1@gmail.com>
2019-11-07 16:14:46 -0800

    add mongoose, connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { UrlController } from './url/url.controller';
 import { UrlModule } from './url/url.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './config/config.service';
+
+let config = new ConfigService(`${process.env.NODE_ENV || 'development'}.env`);
 
 @Module({
-  imports: [ConfigModule, UrlModule],
-  controllers: [AppController, UrlController],
+  imports: [
+    MongooseModule.forRoot(config.get('MONGODB_URI'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    ConfigModule,
+    UrlModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
