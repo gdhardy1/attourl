ca9853569001fe826fa4ddc8d799fabb05486a64
Gregory D Hardy <gdhardy1@gmail.com>
2019-11-07 16:25:17 -0800

    define UrlModule

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { UrlController } from './url.controller';
 import { UrlService } from './url.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UrlSchema } from './schemas/url.schema';
+import { ConfigModule } from 'src/config/config.module';
 
 @Module({
-  providers: [UrlService]
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([{ name: 'Url', schema: UrlSchema }]),
+  ],
+  controllers: [UrlController],
+  providers: [UrlService],
+  exports: [MongooseModule, UrlService],
 })
 export class UrlModule {}
