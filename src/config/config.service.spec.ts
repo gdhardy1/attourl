6bc5e34df46fa6a11c3c36a2bad27d5d86e18149
Gregory D Hardy <gdhardy1@gmail.com>
2019-11-08 14:45:21 -0800

    write ConfigService test

--- src/config/config.service.spec.ts
+++ src/config/config.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigService } from './config.service';
+import { ConfigModule } from './config.module';
 
 describe('ConfigService', () => {
-  let service: ConfigService;
+  let config: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ConfigService],
+      imports: [ConfigModule],
     }).compile();
 
-    service = module.get<ConfigService>(ConfigService);
+    config = module.get<ConfigService>(ConfigService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('ConfigService.get(arg)', () => {
+    it('should return arg value from .env file', () => {
+      expect(config.get('MONGODB_URI')).toBe('TEST-Mongo-URI');
+      expect(config.get('ENV')).toBe('test');
+    });
   });
 });
